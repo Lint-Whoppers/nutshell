727dce7870ec40d1408d538365c7744638c90681
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-08 14:04:18 -0600

    made navbar and added logo and matched colors

--- src/scripts/welcomePage/welcomeHtml.js
+++ src/scripts/welcomePage/welcomeHtml.js
@@ -1,6 +1,14 @@
 export default {
     buildAndAppendWelcomePageHtml: () => {
     const welcomeHtml =  `     
+    <nav id="navBar">
+        <ul class="topnav">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <img class="right" src="src/images/otter1.png">
+        </ul>
+    </nav>
     <div id="welcome-page">
         <h1>Welcome to Otter</h1>
         <button>Sign Up</button>
@@ -12,5 +20,4 @@ const welcomePageContainer = document.querySelector("#page-container")
         welcomePageContainer.innerHTML = welcomeHtml
     }
 }
-
 
