6e5374dd085d203db5242ed75a30f1bbc258e4e9
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-11-13 09:12:04 -0600

    done

--- src/scripts/tasks/domInjector.js
+++ src/scripts/tasks/domInjector.js
@@ -2,27 +2,27 @@
 
 // THis will be a function that allows me to create new dom elements for my portion of the UserMainPage
 
-const taskEntryManager = {
-    makeTaskEntryComponent() {
-return `
-<fieldset>
-<div id="toDo-container" class="header">
-  <h2>ToDo List</h2>
-  <input type="text" id="myInput" placeholder="ToDo">
-  <span onclick="newElement()" class="addBtn">Add</span>
-</div>
+// const taskEntryManager = {
+//     makeTaskEntryComponent() {
+// return `
+// <fieldset>
+// <div id="toDo-container" class="header">
+//   <h2>ToDo List</h2>
+//   <input type="text" id="myInput" placeholder="ToDo">
+//   <span onclick="newElement()" class="addBtn">Add</span>
+// </div>
 
-<ul id="myUL">
-  <li>Hit the gym</li>
-  <li class="checked">Pay bills</li>
-  <li>Meet George</li>
-  <li>Buy eggs</li>
-  <li>Read a book</li>
-  <li>Organize office</li>
-</ul>
-</fieldset>
-   `
-  },
-}
+// <ul id="myUL">
+//   <li>Hit the gym</li>
+//   <li class="checked">Pay bills</li>
+//   <li>Meet George</li>
+//   <li>Buy eggs</li>
+//   <li>Read a book</li>
+//   <li>Organize office</li>
+// </ul>
+// </fieldset>
+//    `
+//   },
+// }
 
-export default taskEntryManager;
+// export default taskEntryManager;
