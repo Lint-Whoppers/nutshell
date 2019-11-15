97307f57bad8e86aecf1b57d1d63a1c0d8ded01d
Sam Pita <samanthakpita@gmail.com>
2019-11-15 10:57:10 -0600

    user auth

---
+++ src/scripts/auth/eventListeners.js
@@ -0,0 +1,13 @@
+// Purpose: The purpose of this file is to handle user authentication for login and logout of application. Author: Sam Pita
+
+const auth = {
+    userLogin() {
+
+    },
+
+    userLogout() {
+
+    }
+}
+
+export default auth
